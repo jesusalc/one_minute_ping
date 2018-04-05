39295819513a48d0564a7c19273ff36c8b84e4eb
jesusalc <jesusalc@gmail.com>
2018-04-05 09:29:50 +0200

    ran ran rubocop and passed spacing and tests

--- lib/one_minute_ping/helper.rb
+++ lib/one_minute_ping/helper.rb
@@ -8,8 +8,6 @@ class Helper
     (finish - start) * 1000.0
   end
 
-  # rubocop:disable Metrics/AbcSize
-  # rubocop:disable Metrics/MethodLength
   def self.check_status
     access_index = 0
     mean_list = []
@@ -24,8 +22,6 @@ class Helper
     end
     mean_list
   end
-  # rubocop:enable Metrics/AbcSize
-  # rubocop:enable Metrics/MethodLength
 
   def self.wait_time(deduction_time_start, ten_milliseconds)
     deduction_elapsed_time = millis_diff(deduction_time_start, Time.now)
@@ -44,8 +40,8 @@ class Helper
   def self.puts_output(elapsed_time, website, mean_list)
     puts "\nServer Hostname:      " + website
     puts "\nTime taken for tests: " + seconds(elapsed_time).to_s +
-         ' seconds'
-    puts 'Time per request:     ' + calculate_average(mean_list).to_s +
+         " seconds"
+    puts "Time per request:     " + calculate_average(mean_list).to_s +
          " [ms] (mean, across all concurrent requests) \n\n"
   end
 
@@ -56,12 +52,11 @@ class Helper
   def self.begin_hide_stdout
     orig_stderr = $stderr.clone
     orig_stdout = $stdout.clone
-    $stderr.reopen File.new('/dev/null', 'w')
-    $stdout.reopen File.new('/dev/null', 'w')
+    $stderr.reopen File.new("/dev/null", "w")
+    $stdout.reopen File.new("/dev/null", "w")
     [orig_stderr, orig_stdout]
   end
 
-  # rubocop:disable Metrics/MethodLength
   def self.hide_stdout
     begin
       orig_stderr, orig_stdout = begin_hide_stdout
@@ -76,5 +71,4 @@ class Helper
     end
     return_value
   end
-  # rubocop:enable Metrics/MethodLength
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,19 @@
-ENV['TEST'] = '1'
+ENV["TEST"] = "1"
 
 # require "simplecov"
 # SimpleCov.start
 
-require 'pp'
-require 'open3'
+require "pp"
+require "open3"
 
-root = File.expand_path('..', __dir__)
+root = File.expand_path("..", __dir__)
 require "#{root}/lib/one_minute_ping"
 
 module Helpers
   def execute(cmd)
-    puts "Running: #{cmd}" if ENV['DEBUG']
+    puts "Running: #{cmd}" if ENV["DEBUG"]
     stdout, stderr, status = Open3.capture3(cmd.to_s)
-    puts stdout if ENV['DEBUG']
+    puts stdout if ENV["DEBUG"]
     [stdout, stderr, status]
   end
 end
