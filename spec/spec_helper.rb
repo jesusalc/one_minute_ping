26ae83f1ee4d0e48adaeadbcc316c62001b1424d
jesusalc <jesusalc@gmail.com>
2018-04-05 09:23:39 +0200

    changed changed helper to use open3 instead of just cmd

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ ENV['TEST'] = '1'
 # SimpleCov.start
 
 require 'pp'
+require 'open3'
 
 root = File.expand_path('..', __dir__)
 require "#{root}/lib/one_minute_ping"
@@ -11,9 +12,9 @@ require "#{root}/lib/one_minute_ping"
 module Helpers
   def execute(cmd)
     puts "Running: #{cmd}" if ENV['DEBUG']
-    out = `#{cmd}`
-    puts out if ENV['DEBUG']
-    out
+    stdout, stderr, status = Open3.capture3(cmd.to_s)
+    puts stdout if ENV['DEBUG']
+    [stdout, stderr, status]
   end
 end
 
