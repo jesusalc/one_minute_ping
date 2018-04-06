dd9662fa08434f8f30485783e36797d67771ae27
jesusalc <jesusalc@gmail.com>
2018-04-06 17:37:12 +0200

    changed changed test to match new specifications with new view and new results data for helper class

--- spec/lib/one_minute_ping/helper_spec.rb
+++ spec/lib/one_minute_ping/helper_spec.rb
@@ -11,20 +11,38 @@ describe Helper do
   before(:all) do
     @start = 10_000.0
     @finish = 653.5
+    @website = 'none.com'
     @mean_list = [0.5000589999981457, 0.5029990000002726,
                   0.5008369999995921, 0.5025150000001304,
                   0.49944399999731104, 0.49976299999980256]
+    @responses = [{ 200 => 0.5000589999981457 }, { 200 => 0.5029990000002726 },
+                  { 500 => 0.5008369999995921 }, { 500 => 0.5025150000001304 },
+                  { 308 => 0.49944399999731104 }, { 308 => 0.49976299999980256 }]
+    @expected_view = "\n" \
+          "Server Hostname:      #{@website}\n" \
+          "\n" \
+          "Counted requests:     6\n" \
+          'Time taken for tests: '
+    @expected_view_two = "seconds\n" \
+          "\n" \
+          "Time for status 200:  501.529 [ms] (mean, only per all responses with status 200)\n" \
+          "Time for status 308:  499.603 [ms] (mean, only per all responses with status 308)\n" \
+          "Time for status 500:  501.676 [ms] (mean, only per all responses with status 500)\n" \
+          "Time per request:     500.936 [ms] (mean, across all concurrent requests) \n\n"
+    @averages = { total_avg: 500.936, count: 6,
+                  group: { 200 => [0.5000589999981457, 0.5029990000002726],
+                           500 => [0.5008369999995921, 0.5025150000001304],
+                           308 => [0.49944399999731104, 0.49976299999980256] },
+                  results: { 200 => 501.529, 308 => 499.603, 500 => 501.676 } }
   end
 
   describe '#view' do
     it 'returns correct result' do
       printed = capture_stdout do
-        Helper.view(Time.at(1_514_761_200.0), 'none.com', @mean_list)
+        Helper.view(Time.at(1_514_761_200.0), @website, @responses)
       end
-      expect(printed).to include("\nServer Hostname:      none.com\n\n")
-      expect(printed).to include('Time taken for tests: ')
-      expect(printed).to include('Time per request:     500.936 [ms] ')
-      expect(printed).to include("(mean, across all concurrent requests) \n\n")
+      expect(printed).to include(@expected_view)
+      expect(printed).to include(@expected_view_two)
     end
   end
 
@@ -43,13 +61,13 @@ describe Helper do
 
   describe '#check_status' do
     Sniffer.disable!
-    let(:mean_list) do
+    let(:responses) do
       Helper.check_status do
         return 'hello'
       end
     end
     it 'returns hello' do
-      expect(mean_list).to eq('hello')
+      expect(responses).to eq('hello')
     end
   end
 
@@ -79,17 +97,15 @@ describe Helper do
 
   describe '#calculate_average' do
     it 'returns correct result' do
-      expect(Helper.calculate_average(@mean_list)).to eq(500.936)
+      expect(Helper.calculate_average(@responses)).to eq(@averages)
     end
   end
 
   describe '#construct_output' do
     it 'returns correct string' do
-      expect(Helper.construct_output(60.027, 'none.com', 500.936))
-        .to eq("\nServer Hostname:      none.com\n\n" \
-                     "Time taken for tests: 60.027 seconds\n" \
-                     'Time per request:     500.936 [ms] ' \
-                     "(mean, across all concurrent requests) \n\n")
+      res = Helper.construct_output(60.027, @website, @averages)
+      expect(res).to include(@expected_view)
+      expect(res).to include(@expected_view_two)
     end
   end
 end
